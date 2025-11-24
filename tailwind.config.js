bfb556a532fe504095d622dda8df7e2564d4af28
aswinckr <aswinckr@gmail.com>
2025-11-24 01:51:51 +0100

    fix build errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {
+      colors: {
+        neutral: {
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#e5e5e5',
+          300: '#d4d4d4',
+          400: '#a3a3a3',
+          500: '#737373',
+          600: '#525252',
+          700: '#404040',
+          750: '#374151',
+          800: '#262626',
+          900: '#171717',
+          950: '#0a0a0a',
+        }
+      },
       typography: {
         DEFAULT: {
           css: {
--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['#minpath', '#minproc', '#minurl']
-    }
-  },
-  resolve: {
-    alias: {
-      '#minpath': 'vfile/lib/minpath.browser.js',
-      '#minproc': 'vfile/lib/minproc.browser.js',
-      '#minurl': 'vfile/lib/minurl.browser.js'
-    }
-  }
+  plugins: [react()]
 })
